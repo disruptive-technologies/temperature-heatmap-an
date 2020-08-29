b8723f3e5af5167bb0fb12071b0546c2c2867e46
kepler <jhgjeraker@protonmail.com>
2020-08-29 10:44:37 +0200

    added more rooms to layout

--- config/layout.py
+++ config/layout.py
@@ -37,6 +37,13 @@ corners = [
     Point(28.0,  4.0),  # 29
     Point(23.0,  4.0),  # 30
     Point(23.0,  0.0),  # 31
+    Point(30.0,  6.0),  # 32
+    Point(33.0,  5.0),  # 33
+    Point(28.0,  0.0),  # 34
+    Point(27.0,  4.0),  # 35
+    Point(29.5,  4.0),  # 36
+    Point(30.5,  0.0),  # 37
+    Point(31.5,  4.0),  # 38
 ]
 
 # walls
@@ -61,20 +68,25 @@ walls = [
     Line(corners[17], corners[18], wall=True), # 17
     Line(corners[18], corners[19], wall=True), # 18
     Line(corners[19], corners[20], wall=True), # 19
-    Line(corners[20], corners[21], wall=True), # 20
+    Line(corners[20], corners[32], wall=True), # 20
     Line(corners[21], corners[22], wall=True), # 21
     Line(corners[19], corners[22], wall=True), # 22
     Line(corners[21], corners[23], wall=True), # 23
-    Line(corners[23], corners[27], wall=True), # 24
+    Line(corners[33], corners[27], wall=True), # 24
     Line(corners[23], corners[24], wall=True), # 25
     Line(corners[24], corners[25], wall=True), # 26
     Line(corners[25], corners[26], wall=True), # 27
     Line(corners[26], corners[27], wall=True), # 28
-    Line(corners[27], corners[28], wall=True), # 29
-    Line(corners[28], corners[29], wall=True), # 30
-    Line(corners[29], corners[30], wall=True), # 31
+    Line(corners[27], corners[38], wall=True), # 29
+    Line(corners[36], corners[29], wall=True), # 30
+    Line(corners[35], corners[30], wall=True), # 31
     Line(corners[30], corners[31], wall=True), # 32
     Line(corners[31], corners[2] , wall=True), # 33
+    Line(corners[31], corners[34], wall=True), # 34
+    Line(corners[29], corners[34], wall=True), # 35
+    Line(corners[34], corners[37], wall=True), # 36
+    Line(corners[28], corners[37], wall=True), # 37
+    Line(corners[26], corners[37], wall=True), # 38
 ]
 
 # sensors
