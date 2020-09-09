71995fac8662aaec64237816dc8d1667e9b8a2d3
jhgjeraker <jhgjeraker@protonmail.com>
2020-09-09 13:43:54 +0200

    generic defaults

--- sensor_stream.py
+++ sensor_stream.py
@@ -2,9 +2,9 @@
 from heatmap.director import Director
 
 # Fill in from the Service Account and Project:
-USERNAME   = "bsvsebr24spg00b250ag"       # this is the key
-PASSWORD   = "09e436a4ab3e44779b36ded5f5ff1386"     # this is the secret
-PROJECT_ID = "br793014jplfqcpoj45g"                # this is the project id
+USERNAME   = "SERVICE_ACCOUNT_KEY"       # this is the key
+PASSWORD   = "SERVICE_ACCOUT_SECRET"     # this is the secret
+PROJECT_ID = "PROJECT_ID"                # this is the project id
 
 # url base and endpoint
 API_URL_BASE  = "https://api.disruptive-technologies.com/v2"
@@ -14,9 +14,6 @@ if __name__ == '__main__':
 
     # initialise Director instance
     d = Director(USERNAME, PASSWORD, PROJECT_ID, API_URL_BASE, t_range=[18, 27], resolution=5)
-
-    # d.update_heatmap()
-    # d.plot_heatmap()
 
     # iterate historic events
     d.run_history()
